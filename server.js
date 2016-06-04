137d9daf58ed1ccd022eb541185332bc2c686b50
beaubbe <gabriel.couvrette@gmail.com>
2016-06-04 19:55:08 -0400

    move events out of Ready; Replace hard-coded name with bot.user.username.

--- server.js
+++ server.js
@@ -7,55 +7,54 @@ var credentials = require('./credentials.json');
 bot.on("ready", function() {
     console.log("Logged in as: " + bot.user.username + " - (" + bot.user.id + ")");
 	bot.setPlayingGame("WWE SMACKDOWN RAW 2016");
-	
-	bot.on("voiceJoin", function(vch, User){
-		if(vch == null || 
-		   User.username == "JOHN CENA" ||  
-		   User.username == "AIRHORN SOLUTIONS")
-			return;
-			
-		console.log(User.username + ' joined!!');
-		
-		bot.joinVoiceChannel(vch, function(err, voiceConnection){
-			console.log("Errors: " + err);
-			console.log("Joined channel" + voiceConnection.server.name);
-			voiceConnection.playFile(media, { volume: 0.1 }, function (error, streamIntent) {
-				streamIntent.on("error", function (error) {
-					console.log("error " + error);
-				});
-
-				streamIntent.on("end", function () {
-					bot.leaveVoiceChannel(vch);
-				});
-			});
-		});
-	});
-   
-	
-	bot.on("voiceLeave", function(vch, User){
-		if(vch == null || 
-		   User.username == "JOHN CENA" ||  
-		   User.username == "AIRHORN SOLUTIONS")
-			return;
-			
-		console.log(User.username + ' joined!!');
-		
-		bot.joinVoiceChannel(vch, function(err, voiceConnection){
-			console.log("Errors: " + err);
-			console.log("Joined channel" + voiceConnection.server.name);
-			voiceConnection.playFile(mediaBye, { volume: 1 }, function (error, streamIntent) {
-				streamIntent.on("error", function (error) {
-					console.log("error " + error);
-				});
-
-				streamIntent.on("end", function () {
-					bot.leaveVoiceChannel(vch);
-				});
-			});
-		});
-	});
 });
 
+bot.on("voiceJoin", function(vch, User){
+   if(vch == null || 
+      User.username == bot.user.username ||  
+      User.username == "AIRHORN SOLUTIONS")
+      return;
+      
+   console.log(User.username + ' joined!!');
+   
+   bot.joinVoiceChannel(vch, function(err, voiceConnection){
+      console.log("Errors: " + err);
+      console.log("Joined channel" + voiceConnection.server.name);
+      voiceConnection.playFile(media, { volume: 0.1 }, function (error, streamIntent) {
+         streamIntent.on("error", function (error) {
+            console.log("error " + error);
+         });
+
+         streamIntent.on("end", function () {
+            bot.leaveVoiceChannel(vch);
+         });
+      });
+   });
+});
+
+
+bot.on("voiceLeave", function(vch, User){
+   if(vch == null || 
+      User.username == "JOHN CENA" ||  
+      User.username == "AIRHORN SOLUTIONS")
+      return;
+      
+   console.log(User.username + ' joined!!');
+   
+   bot.joinVoiceChannel(vch, function(err, voiceConnection){
+      console.log("Errors: " + err);
+      console.log("Joined channel" + voiceConnection.server.name);
+      voiceConnection.playFile(mediaBye, { volume: 1 }, function (error, streamIntent) {
+         streamIntent.on("error", function (error) {
+            console.log("error " + error);
+         });
+
+         streamIntent.on("end", function () {
+            bot.leaveVoiceChannel(vch);
+         });
+      });
+   });
+});
 
 bot.on("error", function(err){
 	console.log("FATAL ERROR!: " + err);
