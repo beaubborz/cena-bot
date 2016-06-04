4ee44d7a794a299299a32a07e0770daf6efe4a6c
beaubbe <gabriel.couvrette@gmail.com>
2016-06-04 19:41:34 -0400

    Added mexican bye.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 var media = "media/john_cena.ogg";
+var mediaBye = "media/bye.mp3";
 var credentials = require('./credentials.json');
 
 bot.on("ready", function() {
@@ -19,6 +20,30 @@ bot.on("ready", function() {
 			console.log("Errors: " + err);
 			console.log("Joined channel" + voiceConnection.server.name);
 			voiceConnection.playFile(media, { volume: 0.1 }, function (error, streamIntent) {
+				streamIntent.on("error", function (error) {
+					console.log("error " + error);
+				});
+
+				streamIntent.on("end", function () {
+					bot.leaveVoiceChannel(vch);
+				});
+			});
+		});
+	});
+   
+	
+	bot.on("voiceLeave", function(vch, User){
+		if(vch == null || 
+		   User.username == "JOHN CENA" ||  
+		   User.username == "AIRHORN SOLUTIONS")
+			return;
+			
+		console.log(User.username + ' joined!!');
+		
+		bot.joinVoiceChannel(vch, function(err, voiceConnection){
+			console.log("Errors: " + err);
+			console.log("Joined channel" + voiceConnection.server.name);
+			voiceConnection.playFile(mediaBye, { volume: 1 }, function (error, streamIntent) {
 				streamIntent.on("error", function (error) {
 					console.log("error " + error);
 				});
