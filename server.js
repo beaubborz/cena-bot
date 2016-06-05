1fa8567868c342cb9f75b9d4022c821fa6a0af67
Maxime Bedard <maxime.bedard@shopify.com>
2016-06-04 20:02:23 -0400

    remove random spaces

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var media = "media/john_cena.ogg";
 var mediaBye = "media/bye.mp3";
 
 bot.on("ready", function() {
-    console.log("Logged in as: " + bot.user.username + " - (" + bot.user.id + ")");
+  console.log("Logged in as: " + bot.user.username + " - (" + bot.user.id + ")");
 	bot.setPlayingGame("WWE SMACKDOWN RAW 2016");
 });
 
@@ -12,21 +12,21 @@ bot.on("voiceJoin", function(vch, User){
   if(vch == null) return;
   if(User.username != bot.user.username) return;
 
-   console.log(User.username + ' joined!!');
+  console.log(User.username + ' joined!!');
 
-   bot.joinVoiceChannel(vch, function(err, voiceConnection){
-      console.log("Errors: " + err);
-      console.log("Joined channel" + voiceConnection.server.name);
-      voiceConnection.playFile(media, { volume: 0.1 }, function (error, streamIntent) {
-         streamIntent.on("error", function (error) {
-            console.log("error " + error);
-         });
-
-         streamIntent.on("end", function () {
-            bot.leaveVoiceChannel(vch);
-         });
+  bot.joinVoiceChannel(vch, function(err, voiceConnection){
+    console.log("Errors: " + err);
+    console.log("Joined channel" + voiceConnection.server.name);
+    voiceConnection.playFile(media, { volume: 0.1 }, function (error, streamIntent) {
+      streamIntent.on("error", function (error) {
+      console.log("error " + error);
       });
-   });
+
+      streamIntent.on("end", function () {
+      bot.leaveVoiceChannel(vch);
+      });
+    });
+  });
 });
 
 
@@ -34,27 +34,26 @@ bot.on("voiceLeave", function(vch, User){
   if(vch == null) return;
   if(User.username != bot.user.username) return;
 
-   console.log(User.username + ' joined!!');
+  console.log(User.username + ' joined!!');
 
-   bot.joinVoiceChannel(vch, function(err, voiceConnection){
-      console.log("Errors: " + err);
-      console.log("Joined channel" + voiceConnection.server.name);
-      voiceConnection.playFile(mediaBye, { volume: 1 }, function (error, streamIntent) {
-         streamIntent.on("error", function (error) {
-            console.log("error " + error);
-         });
-
-         streamIntent.on("end", function () {
-            bot.leaveVoiceChannel(vch);
-         });
+  bot.joinVoiceChannel(vch, function(err, voiceConnection){
+    console.log("Errors: " + err);
+    console.log("Joined channel" + voiceConnection.server.name);
+    voiceConnection.playFile(mediaBye, { volume: 1 }, function (error, streamIntent) {
+      streamIntent.on("error", function (error) {
+        console.log("error " + error);
       });
-   });
+
+      streamIntent.on("end", function () {
+        bot.leaveVoiceChannel(vch);
+      });
+    });
+  });
 });
 
 bot.on("error", function(err){
 	console.log("FATAL ERROR!: " + err);
 });
-
 
 process.on("SIGINT", function () {
   //graceful shutdown
