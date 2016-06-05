b03b635a7cdc0474eeeba5b4db1f42a0d90a848e
beaubbe <gabriel.couvrette@gmail.com>
2016-06-04 22:22:50 -0400

    THEME SONGS!!

--- server.js
+++ server.js
@@ -5,73 +5,73 @@ var express = require('express');
 var app = express();
 var bot = new Discord.Client();
 var TSPath = './theme_songs/';
-var media = "media/hello.ogg";
-var mediaBye = "media/bye.mp3";
+var media = 'media/hello.ogg';
+var mediaBye = 'media/bye.mp3';
 
 var voiceChannel;
 
 bot.on("ready", function() {
-  console.log("Logged in as: " + bot.user.username + " - (" + bot.user.id + ")");
-	bot.setPlayingGame("WWE SMACKDOWN RAW 2016");
+  console.log(`Logged in as: ${bot.user.username} - (${bot.user.id})`);
+	bot.setPlayingGame('WWE SMACKDOWN RAW 2016');
 
   voiceChannel = bot.channels.find(function(ch){
-    return ch.type == 'voice' && ch.name == "General";
+    return ch.type == 'voice' && ch.name == 'General';
   });
-  console.log("Found voice channel: "+voiceChannel.name);
+  console.log(`Found voice channel: ${voiceChannel.name}`);
 });
 
 bot.on("voiceJoin", function(vch, User){
   if(vch == null) return;
   if(User.username == bot.user.username) return;
 
-  console.log(User.username + ' joined!!');
+  console.log(`${User.username} joined!!`);
   var user_theme_song = fs.readdirSync(TSPath).find(function(filename){
-    filename.indexOf(User.id);
+    return filename.includes(User.id);
   });
   if(user_theme_song)
-    PlayFileInChannel(user_theme_song);
+    PlayFileInChannel(`${TSPath}${user_theme_song}`);
   else
     PlayFileInChannel(media, 0.2);
 });
 
 
 bot.on("voiceLeave", function(vch, User){
-  if(vch == null) return;
+  if(!vch) return;
   if(User.username == bot.user.username) return;
 
-  console.log(User.username + ' joined!!');
+  console.log(`${User.username} joined!!`);
   PlayFileInChannel(mediaBye);
-  
+
 });
 
 
-bot.on("message", function(msg) {
-   if(msg.content.toUpperCase() != "THEME SONG" ||
-      msg.attachments == undefined)
+bot.on('message', function(msg) {
+   if(msg.content.toUpperCase() != 'THEME SONG' ||
+      !msg.attachments)
       return;
    var url = msg.attachments[0].url;
    var ext = url.substring(url.lastIndexOf('.') + 1);
    if(!['mp3', 'wav', 'ogg'].includes(ext.toLowerCase()))
    {
-      bot.reply(msg, "I can't play " + ext + " files, bro!");
+      bot.reply(msg, `I can't play ${ext} files, bro!`);
       return;
    }
-   console.log("Theme song request from "+ msg.author.username +" for "+msg.attachments[0].url);
-   
+   console.log(`Theme song request from ${msg.author.username} for ${msg.attachments[0].url}`);
+
    https.get(msg.attachments[0].url, function(data) {
-      var fileToSave = TSPath + msg.author.id + '.' + ext;
+      var fileToSave = `${TSPath}${msg.author.id}.${ext}`;
       var file = fs.createWriteStream(fileToSave);
       data.pipe(file);
     file.on('finish', function() {
       file.close();
-      bot.reply(msg, "Damn, that's a fine theme song!");
+      bot.reply(msg, 'Damn, that\'s a fine theme song!');
       PlayFileInChannel(fileToSave);
     });
    });
 });
 
 bot.on("error", function(err){
-	console.log("FATAL ERROR!: " + err);
+	console.log(`FATAL ERROR!: ${err}`);
 });
 
 app.use('/', express.static(__dirname + '/public'));
@@ -84,7 +84,7 @@ app.listen(server_port, function () {
   );
 });
 
-var PlayFileInChannel = function(filepath, v) {
+var PlayFileInChannel = function(filepath, v = 1.0) {
   if(!voiceChannel)
   {
     console.log("Error! voiceChannel is null.");
@@ -92,14 +92,14 @@ var PlayFileInChannel = function(filepath, v) {
   }
   console.log(filepath);
   bot.joinVoiceChannel(voiceChannel, function(err, voiceConnection) {
-    console.log("Errors: " + err);
-    console.log("Joined channel" + voiceConnection.server.name);
-    voiceConnection.playFile(filepath, { volume: (v ? v:1) }, function (error, streamIntent) {
-      streamIntent.on("error", function (error) {
-        console.log("error " + error);
+    if(err) console.log(`Errors: ${err}`);
+    console.log(`Joined channel ${voiceConnection.server.name}`);
+    voiceConnection.playFile(filepath, { volume: v}, function (error, streamIntent) {
+      streamIntent.on('error', function (error) {
+        console.log(`error: ${error}`);
       });
 
-      streamIntent.on("end", function () {
+      streamIntent.on('end', function () {
         bot.leaveVoiceChannel(voiceChannel);
       });
     });
